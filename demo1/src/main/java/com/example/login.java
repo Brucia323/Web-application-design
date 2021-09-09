ea3ecea0d915c5138e269a362063d4cfec9a571d
周晨阳 <1448517833@qq.com>
2021-09-09 15:28:29 +0800

    Create login.java

---
+++ demo1/src/main/java/com/example/login.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "login", value = "/login")
+public class login extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        if (username == null || "".equals(username.trim()) || password == null || "".equals(password.trim())) {
+            out.println("<h1>用户名或密码为空</h1>");
+        } else if (username.equals("admin") && password.equals("123")) {
+            out.println("<h1>登录成功</h1>");
+        } else {
+            out.println("<h1>用户名或密码错误</h1>");
+        }
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        doGet(req, resp);
+    }
+
+    public void destroy() {
+    }
+}
