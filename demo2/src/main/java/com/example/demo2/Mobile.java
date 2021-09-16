eeed87efbf7d6431bca0ae89f43b995ca765c527
周晨阳 <1448517833@qq.com>
2021-09-16 15:13:38 +0800

    Create Mobile.java

---
+++ demo2/src/main/java/com/example/demo2/Mobile.java
@@ -0,0 +1,37 @@
+package com.example.demo2;
+
+public class Mobile {
+    private int id;
+    private String type;
+    private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Mobile(int id, String type, int price) {
+        this.id = id;
+        this.type = type;
+        this.price = price;
+    }
+}
