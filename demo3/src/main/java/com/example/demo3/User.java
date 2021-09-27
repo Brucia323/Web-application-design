02e8907520d74fe6e5cee5a48dbc308741671b0b
周晨阳 <1448517833@qq.com>
2021-09-27 17:29:35 +0800

    Update User.java

--- demo3/src/main/java/com/example/demo3/User.java
+++ demo3/src/main/java/com/example/demo3/User.java
@@ -1,23 +1,3 @@
-/*create table users
-(
-	id int,
-	name nvarchar(40) not null,
-	password varchar(20) not null
-);
-
-create unique index users_id_uindex
-	on users (id);
-
-create unique index users_name_uindex
-	on users (name);
-
-alter table users
-	add constraint users_pk
-		primary key (id);
-
-alter table users modify id int auto_increment;
-
-*/
 package com.example.demo3;
 
 import javax.servlet.annotation.WebServlet;
