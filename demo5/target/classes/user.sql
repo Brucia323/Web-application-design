38bc85851d982f69e89bbbd0abeb066add0cde76
周晨阳 <1448517833@qq.com>
2021-09-29 20:14:57 +0800

    Create user.sql

---
+++ demo5/target/classes/user.sql
@@ -0,0 +1,21 @@
+create table user
+(
+    id       int,
+    name     nvarchar(40)  not null,
+    password varchar(20)   not null,
+    manager  int default 0 not null
+);
+
+create unique index user_id_uindex
+    on user (id);
+
+create unique index user_name_uindex
+    on user (name);
+
+alter table user
+    add constraint user_pk
+        primary key (id);
+
+alter table user
+    modify id int auto_increment;
+
