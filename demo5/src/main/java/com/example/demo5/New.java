8807a2497db29c627ee2d5ff2dd7f1c4cc6399d4
周晨阳 <1448517833@qq.com>
2021-10-02 20:29:22 +0800

    Update New.java

--- demo5/src/main/java/com/example/demo5/New.java
+++ demo5/src/main/java/com/example/demo5/New.java
@@ -6,9 +6,9 @@ import java.io.RandomAccessFile;
 import java.sql.*;
 
 public class New {
-    private int userId;
-    private String title;
-    private String content;
+    private int userId; // 用户ID
+    private String title;  // 标题
+    private String content; // 内容
 
     public void setUserId(int id) {
         this.userId = id;
@@ -24,32 +24,33 @@ public class New {
 
     public boolean write() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver"); // 加载驱动程序
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             return false;
         }
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HuaTi", "root", "20010323");
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO huati(title, userid, time) VALUES ('" + title + "','" + userId + "','" + new Timestamp(System.currentTimeMillis()) + "')");
-            int result = preparedStatement.executeUpdate();
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HuaTi", "root", "20010323"); // 连接数据库
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO huati(title, userid, time) VALUES ('" + title + "', '" + userId + "', '" + new Timestamp(System.currentTimeMillis()) + "')"); // 新建话题
+            int result = preparedStatement.executeUpdate(); // 执行插入语句
             if (result > 0) {
-                preparedStatement = connection.prepareStatement("SELECT max(id) FROM huati");
-                ResultSet resultSet = preparedStatement.executeQuery();
+                preparedStatement = connection.prepareStatement("SELECT max(id) FROM huati"); // 查询话题ID
+                ResultSet resultSet = preparedStatement.executeQuery(); // 执行查询语句并返回结果集
                 if (resultSet.next()) {
-                    int id = Integer.parseInt(resultSet.getString(1));
+                    int id = Integer.parseInt(resultSet.getString(1)); // 话题ID
                     try {
-                        RandomAccessFile randomAccessFile = new RandomAccessFile("huati" + id + ".txt", "rw");
+                        RandomAccessFile randomAccessFile = new RandomAccessFile("huati" + id + ".txt", "rw"); // 打开文件
                         try {
-                            randomAccessFile.write(content.getBytes());
+                            randomAccessFile.write(content.getBytes()); // 写入文件
                             randomAccessFile.close();
                             resultSet.close();
                             preparedStatement.close();
                             connection.close();
                             return true;
                         } catch (IOException e) {
+                            // 写入文件出错
                             e.printStackTrace();
-                            preparedStatement = connection.prepareStatement("DELETE FROM huati WHERE id='" + id + "'");
+                            preparedStatement = connection.prepareStatement("DELETE FROM huati WHERE id = '" + id + "'"); // 删除记录
                             preparedStatement.executeUpdate();
                             try {
                                 randomAccessFile.close();
@@ -66,8 +67,9 @@ public class New {
                             }
                         }
                     } catch (FileNotFoundException e) {
+                        // 打开文件出错
                         e.printStackTrace();
-                        preparedStatement = connection.prepareStatement("DELETE FROM huati WHERE id='" + id + "'");
+                        preparedStatement = connection.prepareStatement("DELETE FROM huati WHERE id = '" + id + "'"); // 删除记录
                         preparedStatement.executeUpdate();
                         resultSet.close();
                         preparedStatement.close();
