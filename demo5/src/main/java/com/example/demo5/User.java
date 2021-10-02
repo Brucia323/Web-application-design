e4b49fb144d9e030e417f2c282c9281bfe251e61
周晨阳 <1448517833@qq.com>
2021-10-02 20:29:28 +0800

    Update User.java

--- demo5/src/main/java/com/example/demo5/User.java
+++ demo5/src/main/java/com/example/demo5/User.java
@@ -3,10 +3,10 @@ package com.example.demo5;
 import java.sql.*;
 
 public class User {
-    private int id;
-    private String name;
-    private String password;
-    private int isManager;
+    private int id; // 用户ID
+    private String name; // 用户名
+    private final String password; // 密码
+    private int isManager; // 管理员标识
 
     public User(String name, String password) {
         this.name = name;
@@ -25,19 +25,22 @@ public class User {
         this.name = name;
     }
 
+    /**
+     * 登录
+     */
     public boolean login() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver"); // 加载驱动程序
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             return false;
         }
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HuaTi", "root", "20010323");
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user WHERE name='" + name + "' AND password='" + password + "'");
-            ResultSet resultSet = preparedStatement.executeQuery();
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HuaTi", "root", "20010323"); // 连接数据库
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user WHERE name = '" + name + "' AND password = '" + password + "'"); // 查询对应用户名和密码
+            ResultSet resultSet = preparedStatement.executeQuery(); // 执行查询语句并返回结果集
             if (resultSet.next()) {
-                id = Integer.parseInt(resultSet.getString(1));
+                id = Integer.parseInt(resultSet.getString(1)); // 记录用户ID
                 resultSet.close();
                 preparedStatement.close();
                 connection.close();
@@ -54,16 +57,19 @@ public class User {
         }
     }
 
+    /**
+     * 注册
+     */
     public boolean enroll() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver"); // 加载驱动程序
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             return false;
         }
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HuaTi", "root", "20010323");
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO user (name, password, manager) VALUES ('" + name + "','" + password + "','" + isManager + "')");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HuaTi", "root", "20010323"); // 连接数据库
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO user (name, password, manager) VALUES ('" + name + "', '" + password + "', '" + isManager + "')"); // 插入用户记录
             int result = preparedStatement.executeUpdate();
             preparedStatement.close();
             connection.close();
