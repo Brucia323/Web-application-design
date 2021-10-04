f129d9e4980e63bc2e98719d53ec7032e153c9f5
周晨阳 <1448517833@qq.com>
2021-10-04 18:37:11 +0800

    Create EditServlet.java

---
+++ demo5/src/main/java/com/example/demo5/EditServlet.java
@@ -0,0 +1,47 @@
+package com.example.demo5;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet(name = "EditServlet", value = "/EditServlet")
+public class EditServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userid = request.getParameter("userid");
+        String id = request.getParameter("id");
+        String title = request.getParameter("title");
+        String content = request.getParameter("content");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HuaTi", "root", "20010323");
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE huati SET title = '" + title + "' WHERE id = '" + id + "'");
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        RandomAccessFile randomAccessFile = new RandomAccessFile("huati" + id + ".txt", "rw");
+        randomAccessFile.write(content.getBytes());
+        randomAccessFile.close();
+        response.setHeader("refresh", "0;url=main.jsp?id=" + userid + "#" + id);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
