c6e1e4e97bb046e31d563a3e7a487e438e90300b
周晨阳 <1448517833@qq.com>
2021-10-11 20:04:59 +0800

    Create FormCheck.java

---
+++ demo6/src/main/java/com/example/demo6/FormCheck.java
@@ -0,0 +1,33 @@
+package com.example.demo6;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "formcheck", value = "/formcheck")
+public class FormCheck extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo6", "root", "20010323");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user WHERE name = '"+request.getParameter("name")+"'");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
