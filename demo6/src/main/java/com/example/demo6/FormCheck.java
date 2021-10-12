4967f75ba4cedc8f1e163ff6cfd0cbf9b5a16df2
周晨阳 <1448517833@qq.com>
2021-10-12 09:45:25 +0800

    Update FormCheck.java

--- demo6/src/main/java/com/example/demo6/FormCheck.java
+++ demo6/src/main/java/com/example/demo6/FormCheck.java
@@ -1,22 +1,19 @@
 package com.example.demo6;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
 
-@WebServlet(name = "formcheck", value = "/formcheck")
+@WebServlet(name = "FormCheck", value = "/FormCheck")
 public class FormCheck extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -24,8 +21,17 @@ public class FormCheck extends HttpServlet {
             e.printStackTrace();
         }
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo6", "root", "20010323");
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user WHERE name = '"+request.getParameter("name")+"'");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo6", "root",
+                    "20010323");
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("SELECT * FROM user WHERE name = '" + request.getParameter("username") + "'");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()) {
+                out.println("用户名不存在");
+            }
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
