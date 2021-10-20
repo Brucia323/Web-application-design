934dd71245b2b59a02a7d0ae9aa2042241e16802
周晨阳 <1448517833@qq.com>
2021-10-20 10:00:33 +0800

    Create LoginServlet.java

---
+++ demo10/src/main/java/com/example/demo10/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.example.demo10;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+@WebServlet(name = "LoginServlet", value = "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        User user = new User(username, password);
+        try {
+            if (user.login()) {
+                HttpSession httpSession = request.getSession();
+                httpSession.setAttribute("id", user.getId());
+                httpSession.setAttribute("administrator", user.getAdministrator());
+                response.setHeader("refresh","url=index.html");
+            }else {
+                PrintWriter out=response.getWriter();
+                out.println("用户名或密码不正确");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
