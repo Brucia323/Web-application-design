fb698203438c0b42eac40492300220c78f5369e8
周晨阳 <1448517833@qq.com>
2021-10-20 10:00:36 +0800

    Create MySQL.java

---
+++ demo10/src/main/java/com/example/demo10/MySQL.java
@@ -0,0 +1,7 @@
+package com.example.demo10;
+
+public class MySQL {
+    public static String url = "jdbc:mysql://localhost:3306/demo10";
+    public static String user = "root";
+    public static String password = "20010323";
+}
