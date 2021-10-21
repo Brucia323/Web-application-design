331f7916bea917b0d1f155a1a89cc88f215b1f10
周晨阳 <1448517833@qq.com>
2021-10-21 20:12:24 +0800

    Create topics.sql

---
+++ demo10/target/classes/topics.sql
@@ -0,0 +1,17 @@
+-- auto-generated definition
+create table topics
+(
+    id      int auto_increment,
+    userid  int                                  not null,
+    time    datetime   default CURRENT_TIMESTAMP not null,
+    likes   int        default 0                 not null,
+    reply   int        default 0                 not null,
+    sticky  tinyint(1) default 0                 not null,
+    essence tinyint(1) default 0                 not null,
+    constraint topics_id_uindex
+        unique (id)
+);
+
+alter table topics
+    add primary key (id);
+
