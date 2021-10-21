23c9b4ff2f2547d40c6a9ab3cbaa3636114f1ea2
周晨阳 <1448517833@qq.com>
2021-10-21 20:12:00 +0800

    Update ViewTopicServlet.java

--- demo10/src/main/java/com/example/demo10/ViewTopicServlet.java
+++ demo10/src/main/java/com/example/demo10/ViewTopicServlet.java
@@ -1,15 +1,29 @@
 package com.example.demo10;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 
 @WebServlet(name = "ViewTopicServlet", value = "/ViewTopicServlet")
 public class ViewTopicServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        Topics topics = new Topics();
+        try {
+            out.print(topics.viewTopic());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
