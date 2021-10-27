a8327a65e43df91f7cd3393163a70e1b22e3ee55
周晨阳 <1448517833@qq.com>
2021-10-27 08:18:19 +0800

    21.10.27
    修复 已知bug

--- demo10/src/main/webapp/scripts/delete.js
+++ demo10/src/main/webapp/scripts/delete.js
@@ -4,7 +4,6 @@
  * @ZZZCNY
  */
 function deleteTopic(topicid) {
-    $(document).append('<fast-dialog id="example1" class="example-dialog" aria-label="Simple modal dialog" modal="true" hidden><h2>Dialog</h2><p>This is an example dialog.</p><fast-button>Close Dialog</fast-button></fast-dialog>"');
     var select = confirm("确定要删除吗？");
     if (select) {
             $.get("DeleteServlet", { topicid: topicid }, function () {
